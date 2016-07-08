c4ca0f93ffc6fb8b53cec0ef4c4cf1068f24b026
Erich <erich.wappis@uni-graz.at>
2016-07-08 11:29:32 +0200

    Issue #9 master in development for 3.1 version and travis changed

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016070500;
-$plugin->requires  = 2015111600;
+$plugin->requires  = 2016052300;
 $plugin->component = 'filter_embedrc';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.0.0.0';
+$plugin->release   = '3.1.0.0';
